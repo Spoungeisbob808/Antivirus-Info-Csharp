1cbda64f93d39738b39d6328cc448d01b1951ac5
Vladimir <105337731+Spoungeisbob808@users.noreply.github.com>
2022-05-11 08:03:43 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -31,24 +31,15 @@ public class Program
 
     public enum WSC_SECURITY_PROVIDER : int
     {
-        // Represents the aggregation of all firewalls for this computer.
-        WSC_SECURITY_PROVIDER_FIREWALL = 0x1,
-        // Represents the Automatic updating settings for this computer.
-        WSC_SECURITY_PROVIDER_AUTOUPDATE_SETTINGS = 0x2,
-        // Represents the aggregation of all antivirus products for this comptuer.
+    
+        WSC_SECURITY_PROVIDER_FIREWALL = 0x1,  
+        WSC_SECURITY_PROVIDER_AUTOUPDATE_SETTINGS = 0x2,  
         WSC_SECURITY_PROVIDER_ANTIVIRUS = 0x4,
-        // Represents the aggregation of all antispyware products for this comptuer.
-        WSC_SECURITY_PROVIDER_ANTISPYWARE = 0x8,
-        // Represents the settings that restrict the access of web sites in each of the internet zones.
-        WSC_SECURITY_PROVIDER_INTERNET_SETTINGS = 0x10,
-        // Represents the User Account Control settings on this machine.
+        WSC_SECURITY_PROVIDER_ANTISPYWARE = 0x8,  
+        WSC_SECURITY_PROVIDER_INTERNET_SETTINGS = 0x10,      
         WSC_SECURITY_PROVIDER_USER_ACCOUNT_CONTROL = 0x20,
-        // Represents the running state of the Security Center service on this machine.
         WSC_SECURITY_PROVIDER_SERVICE = 0x40,
-
         WSC_SECURITY_PROVIDER_NONE = 0,
-
-        // Aggregates all of the items that Security Center monitors.
         WSC_SECURITY_PROVIDER_ALL = WSC_SECURITY_PROVIDER_FIREWALL |
                                                                 WSC_SECURITY_PROVIDER_AUTOUPDATE_SETTINGS |
                                                                 WSC_SECURITY_PROVIDER_ANTIVIRUS |
